cd6be3e758a04e4090b51271f00afc69787a3ee5
Pcronin170 <patrickryancronin@gmail.com>
2015-08-06 16:17:51 -0400

    Day 2

--- Week1.R
+++ Week1.R
@@ -1,4 +1,5 @@
-
+###Lesson 1 
+setwd("h:/Coursera/R_Programming") 
 
 #Create Vector
 x
@@ -84,6 +85,95 @@ dimnames(m) = list(c("a","b"),c("x","y"))
 #file, header, sep, colClasses, nrows, comment.char, skip, stringsAsFactors
 
 
+###Reading in larger data sets###
+#set comment.char = ""
+#intital <- read.table("datatable.txt",nrows = 100)
+#classes <- sapply(initial,class)
+#tabAll <- read.table(datatable.txt,colClasses = classes)
+
+###Textual Data Formats###
+
+#dput - single object only
+y <- data.frame(a=1,b="a")
+dput(y)
+dput(y, file = "y.r")
+new.y <- dget("y.r")
+new.y
+
+#Dumping
+#designed for mutliple objects
+x <- "foo"
+y <- data.frame(a=1,b=2)
+dump(c("x","y"),file="data.R")
+rm(x,y) #remove variables
+source("data.R")
 
 
+###Interfaces to the outside world###
+#file - opens file
+#gzfile - compressed gzip
+#bzfile - compressed bzip2
+#url - opens connection to webpage
+
+###Subsetting
+x <- list(foo=1:4,bar=0.6)
+x[1] #returns a list
+x[[1]] #returns the sequence (not the list)
+
+x$bar #
+x[["bar"]] #Same as $bar
+x["bar"] #returns list with element bar in it
+x[2]
+
+#Needed to extract multiple elements of a list
+x<- list(foo=1:4,bar = 0.6, baz = "hello")
+x[c(1,3)]
+
+#[[ operator ]] can be sued with computed indicies
+#$can only be used with literal names
+x<- list(foo=1:4,bar = 0.6, baz = "hello")
+name <- "foo"
+x[[name]] #Need double brackets to call computed field
+x$name #does not exist
+x$foo #exists
+
+#Subsetting nested elements of a list
+x<-list(a=list(10,12,14),b=c(3.14,2.81))
+x[[c(1,3)]] #3rd element of the 1st element
+x[[1]][[3]]
+x[1]
+
+###Subsetting Matricies###
+x <- matrix(1:6,2,3)
+x[1,2]#item
+x[2,1]
+x[,2] #Whole column
+#Set drop equal to false if you want a matrix back and not a vector
+#or element
+
+###Remove NA Values###
+x <- c(1,2,NA,4,NA,5)
+bad <- is.na(x)
+x[!bad]
+x <- x[!bad]
+x = na.omit(x)
+
+x <- c(1,2,NA,NA,NA,5)
+y <- c("a","b",NA,"d",NA,"f")
+
+good <- complete.cases(x,y)
+x[good]
+y[good]
+
+###Vectorized languages###
+x <- 1:4
+y <- 6:9
+
+x>2
+x<=2
+y == 8
+x * y
+x / y
+
+#Matrix stuff is different
 
